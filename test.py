96bf0a85be822a9b62c3082fbec98cee3b987f40
Yogesh Jadhav <yogeshjadhav96@gmail.com>
2018-02-06 06:52:38 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from pygresql.pg import DB
-con = DB(dbname='icuat')
-query = "SELECT nspname FROM pg_namespace where nspname = 'test2'"
-get_schema = con.query(query)
-row = get_schema.getresult()
-if row:
-    print('True')
-else:
-    print('false')
